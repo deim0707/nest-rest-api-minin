486ff32632cc2938a8597c3dd8dd15e9a3aa0301
Dmitry <DSParshin@beeline.ru>
2021-06-17 19:28:14 +0300

    Make empty controller and service - TOYS. use nest generate

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
+import { ToysController } from './toys/toys.controller';
+import { ToysService } from './toys/toys.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController],
-  providers: [AppService, ProductsService],
+  controllers: [AppController, ProductsController, ToysController],
+  providers: [AppService, ProductsService, ToysService],
 })
 export class AppModule {}
